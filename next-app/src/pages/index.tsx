c13fbc339f766ff71b53ba74b7822f0c9e4c3347
megancochran <mecochran13@gmail.com>
2023-11-09 12:16:28 -0500

    basic bar chart

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -1,9 +1,63 @@
-import { Flex, Heading } from '@chakra-ui/react'
+import { Box, Flex, Heading } from '@chakra-ui/react'
+import { Doughnut } from 'react-chartjs-2'
+import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Bar } from 'react-chartjs-2'
+import { faker } from '@faker-js/faker'
 
 export default function Home() {
+  ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+  const options = {
+    indexAxis: 'y' as const,
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'right' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Horizontal Bar Chart',
+      },
+    },
+  }
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  }
   return (
     <>
-      <Flex backgroundColor={'greyscale.grey-900'} flexDir={'column'} w={'100vw'}></Flex>
+      <Flex backgroundColor={'greyscale.grey-900'} flexDir={'column'} w={'100vw'}>
+        <Flex boxSize="75rem">
+          <Bar options={options} data={data} />
+        </Flex>
+      </Flex>
     </>
   )
 }
